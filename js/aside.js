c4ca021ea2394dbaa2b6dd48f4c1c83b62936b17
Konstantin <shujskij.konstantin@gmail.com>
2020-11-05 23:51:48 +0200

    Change aside-bar

--- js/aside.js
+++ js/aside.js
@@ -1,19 +1,5 @@
 (function () {
 
-    $('.open-btn').click(function() {
-        $(this).addClass('open-btn_click');
-        $('.close-btn').removeClass('close-btn_click');
-        $('.aside').addClass('aside_open');
-        $('.main').addClass('main_close');
-    });
-
-    $('.close-btn').click(function() {
-      $(this).addClass('close-btn_click');
-      $('.open-btn').removeClass('open-btn_click');
-      $('.aside').removeClass('aside_open');
-      $('.main').removeClass('main_close');
-    });
-
     $('.user__copy').click(function() {
         var $tmp = $("<textarea>");
         $("body").append($tmp);
@@ -22,6 +8,10 @@
         document.execCommand("copy");
         $tmp.remove();
     });    
+
+    $('.switch-btn').click(function() {
+        $('.aside').toggleClass('aside_moble');
+    });
 
 }());
 
