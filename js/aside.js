989c74c108db788314217601dbb478812412a597
Konstantin <shujskij.konstantin@gmail.com>
2020-11-11 19:16:24 +0200

    Скрытие сайт-бара в моб. версии. Исправление бага со скроломм таблицы

--- js/aside.js
+++ js/aside.js
@@ -4,15 +4,15 @@
         let imgClass = $img.attr('class');
         let imgURL = $img.attr('src');
         $.get(imgURL, function(data) {
-          let $svg = $(data).find('svg');
-          if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-          }
-          $svg = $svg.removeAttr('xmlns:a');
-          if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-          }
-          $img.replaceWith($svg);
+            let $svg = $(data).find('svg');
+            if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+            $svg = $svg.removeAttr('xmlns:a');
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+              $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+            $img.replaceWith($svg);
         }, 'xml');
     });
    
@@ -26,12 +26,12 @@
     });    
 
     $('.user__id').click(function() {
-      let $tmp = $("<textarea>");
-      $("body").append($tmp);
-    
-      $tmp.val($(this).text()).select();
-      document.execCommand("copy");
-      $tmp.remove();
+        let $tmp = $("<textarea>");
+        $("body").append($tmp);
+      
+        $tmp.val($(this).text()).select();
+        document.execCommand("copy");
+        $tmp.remove();
     });    
 
     $('.switch-btn').click(function() {
@@ -41,6 +41,19 @@
 
     $('.map-aside__switch-btn').click(function() {
         $(this).toggleClass('map-aside__switch-btn_service');
+    });
+
+    $('.open-btn').click(function() {
+        $('.aside').removeClass('aside_moble');
+        $('.aside').addClass('aside_open');
+    });
+
+    $(document).click( function(e){
+        if ( $(e.target).closest('.aside').length || $(e.target).closest('.open-btn').length ) {
+            return;
+        }
+
+        $('.aside').removeClass('aside_open');
     });
 
 }());
